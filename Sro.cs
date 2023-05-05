2261f86f12108ca9a0bd3255335d2f460582d972
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 14:58:47 +0530

    Add files via upload

---
+++ Sro.cs
@@ -0,0 +1,20 @@
+using System;
+namespace StaticReadOnly
+{
+    class Program
+    {
+
+
+     static readonly int id;
+
+     static Program(){
+        id=09890;
+        Console.WriteLine(id);
+     }
+
+        static void Main(string [] args){
+Program numm= new Program();
+
+        }
+    }
+}
---
+++ Stconst.cs
@@ -0,0 +1,11 @@
+using System;
+namespace Constant
+{
+    class Program{
+        const int num=22;
+        public static void Main()
+        {
+           Console.WriteLine(num);
+        }
+    }
+}

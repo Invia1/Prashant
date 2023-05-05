76aad1dee5373d57bb7906f1ee1beda6d6f9c5ae
Invia1 <132645377+Invia1@users.noreply.github.com>
2023-05-05 16:35:47 +0530

    Update Reptdnum.cs

--- Reptdnum.cs
+++ Reptdnum.cs
@@ -1,28 +1,25 @@
 using System;
-    class Reptdstr
+class Duplrtd
+{
+    static void Main(string[] args)
     {
-         static void Main(string[] args)
+        Console.WriteLine("Enter a String");
+        String str=Console.ReadLine();
+        int count=0;
+        char[] strArray=str.ToCharArray();
+        Console.WriteLine("Duplicate characters in string");
+        for(int i=0; i<strArray.Length; i++)
         {
-            string str=" ";
-            Console.WriteLine("Enter the String");
-            str =Console.ReadLine();
-            while(str.Length > 0)
+            for(int j=i+1; j<strArray.Length; j++)
             {
-                int count=0;
-                Console.WriteLine(str[0]+" ");
-            for(int i=0; i<str.Length; i++)
-            {
-                
-                    if(str[0]==str[i])
-                    {
-                        count++;
-                    }
+                if(strArray[i]==strArray[j])
+                {
+                    Console.WriteLine(strArray[j]);
+                    count++;
+                    break;
+                }
             }
-                    Console.WriteLine(count);
-                    str=str.Replace(str[0].ToString(), string.Empty);
-                    Console.ReadLine();
-            }
-            
         }
+        Console.WriteLine("Total duplicate characters found:"+count);
     }
-
+}
